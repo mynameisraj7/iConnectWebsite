643c0e878f8754825d653d608b06a33b38d842e6
Aditya Thakur <Aditya@Mac.lan>
2026-02-07 20:45:45 +0530

    Updated changes

--- src/pages/Aboutussections/Manageaboutus.jsx
+++ src/pages/Aboutussections/Manageaboutus.jsx
@@ -1,9 +1,11 @@
 import "./Manageaboutus.css";
-// import bigcardimage from "../../assets/bigcard.png";
+import bigcardimage from "../../assets/bigcard.png";
 //import manprofile from "../../assets/manprofile.jpg";
-import opsang from "../../assets/opsangwansir.jpg";
-import vimal from "../../assets/vimal.jpeg";
-// import sureshkumar from "../../assets/sureshkumar.jpg";
+import opsang from "../../assets/opdirector.jpeg";
+import vimal from "../../assets/vimalk.jpeg";
+import sunil from "../../assets/sunil.jpeg";
+import vijay from "../../assets/Vijay.jpeg";
+import rajendar from "../../assets/rajendar.jpeg";
 
 function Manageaboutus(){
     return(
@@ -29,23 +31,23 @@ function Manageaboutus(){
                     <p>Deputy Director PDUIIC</p>
                  </div>
             </div>
-            {/* <div className="fourcardcontainer">
+            <div className="fourcardcontainer">
                  <div className="smallcard">
-                    <img src={bigcardimage} alt="about section" loading="lazy"/>
-                    <h2>Someone Sharma</h2>
-                    <p>Coordinator</p>
+                    <img src={sunil} alt="about section" loading="lazy"/>
+                    <h2>Dr. Sunil Kumar</h2>
+                    <p>Coordinator PDUIIC</p>
                  </div>
                  <div className="smallcard">
-                    <img src={bigcardimage} alt="about section" loading="lazy"/>
-                    <h2>Someone Sharma</h2>
-                    <p>Coordinator</p>
+                    <img src={vijay} alt="about section" loading="lazy"/>
+                    <h2>Dr. Vijay Pal Singh</h2>
+                    <p>Coordinator Idea Lab</p>
                  </div>
                  <div className="smallcard">
-                    <img src={bigcardimage} alt="about section" loading="lazy"/>
-                    <h2>Someone Sharma</h2>
-                    <p>Coordinator</p>
+                    <img src={rajendar} alt="about section" loading="lazy"/>
+                    <h2>Dr. Rajender Singh</h2>
+                    <p>Coordinator PDUIIC</p>
                  </div>
-            </div> */}
+            </div>
         </section>
         </>
     );
