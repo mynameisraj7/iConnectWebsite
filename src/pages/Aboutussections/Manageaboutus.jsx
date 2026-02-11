3d0618dee7e07415d5317a5a2d0d9455f6a224c8
Aditya Thakur <Aditya@Mac.lan>
2026-02-11 13:00:38 +0530

    Initial React project

--- src/pages/Aboutussections/Manageaboutus.jsx
+++ src/pages/Aboutussections/Manageaboutus.jsx
@@ -6,6 +6,10 @@ import vimal from "../../assets/vvimal.jpeg";
 import sunil from "../../assets/ssunil.jpeg";
 import vijay from "../../assets/vvijay.jpeg";
 import rajendar from "../../assets/rajenderr.jpeg";
+import ajay from "../../assets/ajaykumar.jpeg";
+import narender from "../../assets/narender.jpeg";
+import sandeep from "../../assets/sandeep.jpeg";
+import sumit from "../../assets/sumit.jpeg";
 
 function Manageaboutus(){
     return(
@@ -46,6 +50,28 @@ function Manageaboutus(){
                     <img src={rajendar} alt="about section" loading="lazy"/>
                     <h2>Dr. Rajender Singh</h2>
                     <p>Coordinator PDUIIC</p>
+                 </div>
+            </div>
+             <div className="fourcardcontainer">
+                 <div className="smallcard">
+                    <img src={ajay} alt="about section" loading="lazy"/>
+                    <h2>Mr. Ajay Kumar</h2>
+                    <p>Coordinator Idea Lab</p>
+                 </div>
+                 <div className="smallcard">
+                    <img src={narender} alt="about section" loading="lazy"/>
+                    <h2>Dr. Narender Kumar</h2>
+                    <p>Coordinator Idea Lab</p>
+                 </div>
+                 <div className="smallcard">
+                    <img src={sandeep} alt="about section" loading="lazy"/>
+                    <h2>Dr. Sandeep Jindal</h2>
+                    <p>Coordinator Idea Lab</p>
+                 </div>
+                 <div className="smallcard">
+                    <img src={sumit} alt="about section" loading="lazy"/>
+                    <h2>Dr. Sumit Sharma</h2>
+                    <p>Coordinator Idea Lab</p>
                  </div>
             </div>
         </section>
